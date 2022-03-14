ebef9db617fbc3624837590d74f3000e511a3b4b
Austin A <austinandrews89@gmail.com>
2022-03-14 12:51:59 -0600

    Started added some Inquirer prompts. Pending move to index.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+
+
+const mainMenuArray = [
+    "View all Departments",
+    "View all Roles",
+    "View all Employees",
+    "Add a Department",
+    "Add a Role",
+    "Add an Employee",
+    "Update Employeen Role"
+];
+
+const inquireObject = {
+    mainMenu: [
+        {
+            type: 'list',
+            name: "mainMenu",
+            message: "Select an Option",
+            choices: mainMenuArray
+        }
+],
+    addDepartment: [
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Department ID'        
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter Department Name'
+        }
+],
+    addRole: {
+
+    },
+    addEmployee: {
+
+    },
+    updateEmployee: {
+
+    }
+};
